e201d9d8987dd64674e445f22532cd8ba314a5a6
sd2001 <im.swarnabha2001@gmail.com>
2021-06-11 11:12:18 +0530

    🔥 🎉 Added new route for all Transactions

--- routes/transaction.py
+++ routes/transaction.py
@@ -22,15 +22,20 @@ async def new_issue(transaction: pm.Transactions, db: orm.Session=Depends(serv.g
     if db_user.warning == True:
         raise HTTPException(status_code=403, detail="Member Debt exceeds Limit, Forbidden!")
     
-    
+    db_book = db.query(sql.Books).filter(sql.Books._bookID == transaction.book_id).first()
+    if db_book.rem_stock == 0:
+        raise HTTPException(status_code=406, detail="Book Out of Stock!")
+        
     new_transaction = sql.Transactions(member_id=transaction.member_id, book_id=transaction.book_id)
+    db_book.rem_stock -= 1
     db.add(new_transaction)
     db.commit()
     db.refresh(new_transaction)
+    db.refresh(db_book)
     return new_transaction
 
 @app.put('/transaction/new_issue/{id}', response_model=pm.Transactions)
-async def return_issue(id:int, transaction: pm.Transactions, db: orm.Session=Depends(serv.get_db)):
+async def return_issue(id:int, transaction: pm.Transactions, db: orm.Session=Depends(serv.get_db)):    
     db_transac = db.query(sql.Transactions).filter(sql.Transactions._id == id).first()
     if db_transac is None:
         raise HTTPException(status_code=404, detail="This Transaction ID does not exist!")
@@ -38,6 +43,9 @@ async def return_issue(id:int, transaction: pm.Transactions, db: orm.Session=Dep
         db_transac.returned = True
         db.commit()
         db.refresh(db_transac) 
+    
+    db_book = db.query(sql.Books).filter(sql.Books._bookID == db_transac.book_id).first()
+    db_book.rem_stock += 1
     
     db_transac.pay = transaction.pay
     db_member_id = db_transac.member_id
@@ -53,4 +61,10 @@ async def return_issue(id:int, transaction: pm.Transactions, db: orm.Session=Dep
     db.commit()
     db.refresh(db_transac)    
     db.refresh(db_user)    
+    db.refresh(db_book)    
+    return db_transac
+
+@app.get('/transactions', response_model=List[pm.Transactions])
+async def get_transactions(db: orm.Session=Depends(serv.get_db)):
+    db_transac = db.query(sql.Transactions).all()
     return db_transac
