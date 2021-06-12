3a2c65037ff09c41320b1e705133119b351e916d
sd2001 <im.swarnabha2001@gmail.com>
2021-06-12 20:51:43 +0530

    ✅ Updated positioning of the plots

--- charts/combined_trace.py
+++ charts/combined_trace.py
@@ -10,8 +10,8 @@ def final_plots(members, spend, books, issues):
     fig1 = ms.member_bar_plot(members, spend)
     fig2 = ms.member_pie_plot(members, spend)
     
-    fig3 = bu.book_pie_plot(books, issues)
-    fig4 = bu.book_line_plot(books, issues)
+    fig3 = bu.book_line_plot(books, issues)
+    fig4 = bu.book_pie_plot(books, issues)
     
     with open('templates/chart.html', 'w') as f:
         f.close()
