9032117412f01fa27db3c2144fda098e72044aaa
sd2001 <im.swarnabha2001@gmail.com>
2021-06-12 11:51:21 +0530

    🐛 Configure templates and staticfiles

--- services.py
+++ services.py
@@ -1,8 +1,11 @@
 from config.db import Base, engine, SessionLocal
 from models import sql_models as sql
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 
 def create_db():
-	return sql.Base.metadata.create_all(bind=engine)
+    return sql.Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
@@ -10,3 +13,8 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+def configure_templates(app):
+    templates = Jinja2Templates(directory="templates")
+    app.mount("/static", StaticFiles(directory="static"), name="static")
+    return templates
