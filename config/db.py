22531c60dd9fc146dc5e7040a7465174c6bd9f57
sd2001 <im.swarnabha2001@gmail.com>
2021-06-12 10:52:20 +0530

    🐛 ⬆️  Updated database config

--- config/db.py
+++ config/db.py
@@ -6,7 +6,7 @@ import os
 import psycopg2
 
 load_dotenv()
-DATABASE_URL = os.environ.get['DATABASE_URL']
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 meta = MetaData()
