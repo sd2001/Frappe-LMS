9b69199416355f9153c5b3e8a172b873c8c603d9
sd2001 <im.swarnabha2001@gmail.com>
2021-06-12 11:50:08 +0530

    ⬆️ 🚧 Updated static and template configs

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes import member, books, transaction, reports
 import services as serv
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
-
-serv.create_db()
+templates = serv.configure_templates(app)
 
 app.include_router(member.app)
 app.include_router(books.app)
 app.include_router(transaction.app)
 app.include_router(reports.app)
+
+
+@app.get('/', include_in_schema=False)
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
