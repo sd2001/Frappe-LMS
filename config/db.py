b4843cb3ff1f87ef1edb5b612b7fe5d831183d79
sd2001 <im.swarnabha2001@gmail.com>
2021-06-12 12:21:15 +0530

    🚑 Sqlalchemy postgres dialects fix

--- config/db.py
+++ config/db.py
@@ -7,6 +7,8 @@ import psycopg2
 
 load_dotenv()
 DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 meta = MetaData()
