3d9ae26d18478166b546c0de6d9e20e8d63f56c2
sd2001 <im.swarnabha2001@gmail.com>
2021-06-12 09:43:04 +0530

    ✨ Added Route path for Charts

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routes import member, books, transaction
+from routes import member, books, transaction, reports
 import services as serv
 
 app = FastAPI()
@@ -9,3 +9,4 @@ serv.create_db()
 app.include_router(member.app)
 app.include_router(books.app)
 app.include_router(transaction.app)
+app.include_router(reports.app)
