6e796bb109c1e8907603494a16feec0c7a653e6c
sd2001 <im.swarnabha2001@gmail.com>
2021-06-12 10:27:41 +0530

    🔒️ Changes in db configs

--- config/db.py
+++ config/db.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine, MetaData
 import sqlalchemy.ext.declarative as declarative
 import sqlalchemy.orm as orm
+from dotenv import load_dotenv
+import os
+import psycopg2
 
-engine = create_engine('sqlite:///./data.db', connect_args={"check_same_thread": False})
+load_dotenv()
+DATABASE_URL = os.environ.get['DATABASE_URL']
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 meta = MetaData()
 # conn = engine.connect()
 
